31708d5a8757d66af6ca5af0bc4a59d8b1a58de6
zaraobel <z.belcheva@student.rug.nl>
2024-10-02 08:39:03 +0200

    Remove Stripe API key from code

--- backend/routes/pay.js
+++ backend/routes/pay.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router(); // Create an Express router
 
 // In your backend server (Node.js/Express)
-const stripe = require('stripe')('sk_test_51PlpmJRvF9JCAQqQFSC1cPw0EE492BETTp3yiMMvJ8l7aR7bBvOOtxYSARgMBRz0VfX7qVXlbk8PQJMkGvd9fFkC00YYM3vF5x');
+const stripe = require('stripe')('');
 
 router.post('/create-payment-intent', async (req, res) => {
   const { amount } = req.body;
